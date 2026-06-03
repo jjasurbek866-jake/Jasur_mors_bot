ab200ff53310ded825d3bcdbc366b1a651f7813a
Jake <jjasurbek866@gmail.com>
2026-06-03 14:37:35 +0500

    Eski xotira va reklamalarni butunlay tozalash

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ threading.Thread(target=run_server, daemon=True).start()
 # ====================================================
 
 # ⚠️ BOT TOKENINGIZNI SHU YERGA YOZING
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = "8964012400:AAE4QLsxhG9gbKjmCz-GOpMh17gMNH77P2E"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
